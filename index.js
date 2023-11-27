10684ca28920e1f744e7a4061a08da2a06d41ce0
askholani <askholani210@gmail.com>
2023-11-27 10:25:14 +0700

    coba lagi

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// const express = require('express')
-// const dotenv = require('dotenv')
+import app from './index'
 
-// dotenv.config()
-
-// const app = express()
-// const port = process.env.PORT
-
-// app.get('/', (req, res) => {
-//   res.send('Express + TypeScript Server')
-// })
-
-// app.listen(port, () => {
-//   console.log(`[server]: Server is running at http://localhost:${port}`)
-// })
+export default app
