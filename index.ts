04f17a3d90371714d085371552fd5f0c5977190c
askholani <askholani210@gmail.com>
2023-11-27 11:01:40 +0700

    1

--- index.ts
+++ index.ts
@@ -1,38 +1,49 @@
-import express, { Express } from 'express'
-import dotenv from 'dotenv'
-import RouterAlternatif from './api/routes/alternatif'
-import RouterKriteria from './api/routes/kriteria'
-import RouterRentangSkor from './api/routes/rentangSkort'
-import cors from 'cors'
-import RouterResult from './api/routes/result'
-import RouteMatrix from './api/routes/matrix'
+// import express, { Express } from 'express'
+// import dotenv from 'dotenv'
+// import RouterAlternatif from './api/routes/alternatif'
+// import RouterKriteria from './api/routes/kriteria'
+// import RouterRentangSkor from './api/routes/rentangSkort'
+// import cors from 'cors'
+// import RouterResult from './api/routes/result'
+// import RouteMatrix from './api/routes/matrix'
 
-dotenv.config()
+// dotenv.config()
 
-const app: Express = express()
-const port = process.env.PORT
+// const app: Express = express()
+// const port = process.env.PORT
 
-app.use(express.json())
+// app.use(express.json())
 
-// const corsOptions = {
-//   origin: [
-//     'http://localhost:3000',
-//     'http://127.0.0.1:5500',
-//     'http://127.0.0.1:5501',
-//   ], // Izinkan asal ini
-//   optionsSuccessStatus: 200,
-// }
+// // const corsOptions = {
+// //   origin: [
+// //     'http://localhost:3000',
+// //     'http://127.0.0.1:5500',
+// //     'http://127.0.0.1:5501',
+// //   ], // Izinkan asal ini
+// //   optionsSuccessStatus: 200,
+// // }
 
-// app.use(cors(corsOptions))
+// // app.use(cors(corsOptions))
 
-app.use('/api/alternatif', RouterAlternatif)
-app.use('/api/kriteria', RouterKriteria)
-app.use('/api/rentang', RouterRentangSkor)
-app.use('/api/matrix', RouteMatrix)
-app.use('/api/result', RouterResult)
+// app.use('/api/alternatif', RouterAlternatif)
+// app.use('/api/kriteria', RouterKriteria)
+// app.use('/api/rentang', RouterRentangSkor)
+// app.use('/api/matrix', RouteMatrix)
+// app.use('/api/result', RouterResult)
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+// app.listen(port, () => {
+//   console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+// })
+
+// export default app
+
+import express, { Application, Request, Response, NextFunction } from 'express'
+
+const app: Application = express()
+const port: Number = 4000
+
+app.use('/api/health', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ status: '200' })
 })
 
-export default app
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
